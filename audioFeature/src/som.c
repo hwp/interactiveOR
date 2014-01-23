2009879bc942404c19ca88d98ac9c3627fa195e3
Weipeng He <heweipeng@gmail.com>
2014-01-23 14:10:48 +0100

    SOM data structure, memory allocation and mapping

---
+++ audioFeature/src/som.c
@@ -0,0 +1,69 @@
+// som.c
+// Self Organizing Map
+//
+// Author : Weipeng He <heweipeng@gmail.com>
+// Copyright (c) 2014, All rights reserved.
+
+#include "som.h"
+
+#include <stdlib.h>
+#include <math.h>
+
+// Euclidean Distance
+double f_euc_dis(double* f1, double* f2, size_t size) {
+  double s = 0;
+  size_t i;
+  // TODO floating-point summation problem ignored.
+  // Assume size is small.
+  for (i = 0; i < size; i++) {
+    s += (f1[i] - f2[i]) * (f1[i] - f2[i]);
+  }
+
+  return sqrt(s);
+}
+
+// Feature Distance
+double (*fdistance)(double*, double*, size_t) = f_euc_dis;
+
+SOM* som_alloc(size_t rows, size_t cols, size_t dims) {
+  double* w = malloc(rows * cols * dims * sizeof(double));
+  if (w == NULL) {
+    return NULL;
+  }
+
+  SOM* r = malloc(sizeof(SOM));
+  if (r == NULL) {
+    free(w);
+    return NULL;
+  }
+  
+  r->rows = rows;
+  r->cols = cols;
+  r->dims = dims;
+  r->weight = w;
+
+  return r;
+}
+
+void som_free(SOM* net) {
+  free(net->weight);
+  free(net);
+}
+
+int som_map(SOM* net, double* data) {
+  size_t i, j;
+  double mind = INFINITY;
+  int r = 0;
+  for (i = 0; i < net->rows; i++) {
+    for (j = 0; j < net->cols; j++) {
+      double d = fdistance(GET_WEIGHT(net, i, j), data, net->dims);
+      if (d < mind) {
+        mind = d;
+        r = i * net->cols + j;
+      }
+    }
+  }
+
+  return r;
+}
+
---
+++ audioFeature/src/som.h
@@ -0,0 +1,52 @@
+// som.h
+// Self Organizing Map
+//
+// Author : Weipeng He <heweipeng@gmail.com>
+// Copyright (c) 2014, All rights reserved.
+
+#ifndef SOM_H_
+#define SOM_H_
+
+#include <stdlib.h>
+
+typedef struct {
+  size_t rows;  // Number of rows
+  size_t cols;  // Number of Columns
+  size_t dims;  // Input Dimension
+
+  double* weight;
+} SOM;
+
+#define GET_WEIGHT(s, r, c) \
+  s->weight + ((r * s->cols + c) * s->dims)
+
+/**
+ * Allocate memory for SOM.
+ * Set rows, cols and dims. However, weight is not initialized.
+ */
+SOM* som_alloc(size_t rows, size_t cols, size_t dims);
+
+/**
+ * Free the allocated memory
+ */
+void som_free(SOM* net);
+
+/**
+ * Train with given data (dimension = length * net->dims).
+ * The memory for net should be allocated before calling
+ * this function.
+ * Whole training data will be traversed iters times.
+ */
+void som_train(SOM* net, double* data, size_t length,
+    size_t iters);
+
+/**
+ * Given on sample of data (dimension = net->dims).
+ * maps to a neuron.
+ * return the ID of the neuron, that is 
+ *   row * # of columns + colummn.
+ */
+int som_map(SOM* net, double* data);
+
+#endif  // SOM_H_
+
