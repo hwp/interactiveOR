9d2c161a8204a80dbe9ded740a532c7e52d2a40e
Weipeng He <heweipeng@gmail.com>
2015-04-18 13:06:59 +0200

    update run test script

--- classify/scripts/run_hmm_test.sh
+++ classify/scripts/run_hmm_test.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 usage() {
-  echo "Usage: $0 [-i (0:random|1:kmeans)] [-u unseen] -p hmm_test_path data_dir" >&2
+  echo "Usage: $0 [-i (0:random|1:kmeans)] [-u unseen] [-c cov_diag] -p hmm_test_path data_dir" >&2
   exit 1
 }
 
 unseen=1
+cov_diag=1
 initm=0
-while getopts "p:u:i:" opt; do
+while getopts "p:u:i:c:" opt; do
   case $opt in
     p)
       prog=$OPTARG
@@ -17,6 +18,9 @@ while getopts "p:u:i:" opt; do
       ;;
     i)
       initm=$OPTARG
+      ;;
+    c)
+      cov_diag=$OPTARG
       ;;
     *)
       usage
@@ -46,17 +50,18 @@ for ddir in $ddirs; do
   echo $ddir
   dim=`grep Dimension $ddir/README | awk '{print $2}'`
   name=`echo $ddir | sed 's:^.*/\([^/][^/]*\)/*$:\1:'` 
-  fparam="-d $dim -u $unseen -i $initm -c 1"
+  fparam="-d $dim -u $unseen -i $initm -c $cov_diag"
+  errlog=${name}_errlog
 
   echo $name
 
-  > errlog
+  > $errlog
   for ((i = 0; i < ${#tparam[@]}; i++)); do
     tp=${tparam[$i]}
     echo "  $tp"
     file="${name}_hmm_${tp//-/}"
-    file="${file// /_}"
-    $prog $fparam $tp $ddir 2>> errlog > $file
+    file="${file// /}"
+    $prog $fparam $tp $ddir 2>> $errlog > $file
   done
 done
 
